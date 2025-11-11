3978a7f0b396d69c3d440bec85552d90b3599979
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-11-11 23:07:30 +0530

    but i see some error i try everything to solve

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
+import java.util.Scanner;
 
+// import com.driver.*;
 public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       RWOnly s1 = new RWOnly();
+       String s = sc.next();
+       s1.setName(s);
+       System.out.println(s1.getName());
+       sc.close();
+    }
   
 }
